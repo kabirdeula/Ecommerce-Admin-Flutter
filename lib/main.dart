6cdf5182c58eedc4078131ebd822be3cfbf73417
Kabir Deula <kabirdeula167@gmail.com>
2023-11-12 10:33:05 +0545

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:ecommerce_admin/screens/screen.home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        useMaterial3: true,
+      ),
+      home:const HomeScreen(),
+    );
+  }
+}
+
---
+++ lib/screens/product/screen.create_product.dart
@@ -0,0 +1,8 @@
+class CreateProduct extends StatelessWidget {
+  const CreateProduct({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
