3b59fa40c62445f632ef055e168c093a70e86339
Kabir Deula <kabirdeula167@gmail.com>
2023-11-19 13:15:39 +0545

    added style for button

--- lib/screens/screen.home.dart
+++ lib/screens/screen.home.dart
@@ -1,3 +1,5 @@
+import 'package:ecommerce_admin/screens/product/screen.create_product.dart';
+import 'package:ecommerce_admin/styles/button_style.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -7,7 +9,25 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: const Text('Daraz Lite Admin'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        centerTitle: true,
+      ),
+      body: Center(
+        child: ElevatedButton.icon(
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) {
+                  return const CreateProduct();
+                },
+              ),
+            );
+          },
+          style: CustomButtonStyle.buttonStyle,
+          icon: const Icon(Icons.add),
+          label: const Text('Create Product'),
+        ),
       ),
     );
   }
